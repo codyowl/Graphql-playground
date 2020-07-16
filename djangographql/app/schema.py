52f4299fd4594d5d553c5aa7560264a86d041b1c
codyowl <codyowl@gmail.com>
2020-07-16 20:50:35 +0530

    top level schema added and requirements.txt added

--- djangographql/app/schema.py
+++ djangographql/app/schema.py
@@ -13,5 +13,13 @@ class AuthorType(DjangoObjectType):
 
 
 class Query(object):
-	all_categories = graphene.List(BookType)
-    all_ingredients = graphene.List(AuthorType)				
+	all_books = graphene.List(BookType)
+    all_authors = graphene.List(AuthorType)	
+
+    def resolve_all_books(self, info, **kwargs):
+        return Book.objects.all()
+
+    def resolve_all_authors(self, info, **kwargs):
+        # We can easily optimize query count in the resolve method
+        return Author.objects.select_related('book').all()
+			
